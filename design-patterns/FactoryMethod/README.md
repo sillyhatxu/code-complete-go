09089c6ec5d65ffaca65f0e8243944a2a67fb26a
sillyhat <xushikuan1@163.com>
2020-07-14 23:38:05 +0800

    add Factory Method

---
+++ design-patterns/FactoryMethod/README.md
@@ -0,0 +1,5 @@
+# Factory Method
+
+```
+Creates an instance of several derived classes. Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.
+```
---
+++ design-patterns/FactoryMethod/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/sillyhatxu/code-complete-go/design-patterns/FactoryMethod/fm"
+	"os"
+)
+
+func main() {
+	doc := &fm.Document{
+		ShapeFactories: []fm.ShapeFactory{
+			&fm.CircleFactory{},
+			&fm.RactangleFactory{},
+		},
+	}
+	err := doc.Draw(os.Stdout)
+	if err != nil {
+		panic(err)
+	}
+}
