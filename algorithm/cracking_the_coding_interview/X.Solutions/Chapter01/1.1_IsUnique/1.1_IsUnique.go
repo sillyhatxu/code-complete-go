85e1b355b77bf6208d41832fc7b00727f0a097ca
sillyhat <xushikuan1@163.com>
2020-07-19 09:17:22 +0800

    add Is Unique

--- algorithm/cracking_the_coding_interview/X.Solutions/Chapter01/1.1_IsUnique/1.1_IsUnique.go
+++ algorithm/cracking_the_coding_interview/X.Solutions/Chapter01/1.1_IsUnique/1.1_IsUnique.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+func IsUniqueChars1(input string) bool {
+	if len(input) > 128 {
+		return false
+	}
+	check := make(map[int32]bool)
+	for _, c := range input {
+		_, ok := check[c]
+		if ok {
+			return false
+		}
+		check[c] = true
+	}
+	return true
+}
 
+func IsUniqueChars2(input string) bool {
+	if len(input) > 128 {
+		return false
+	}
+	check := make([]int32, 128)
+	for _, c := range input {
+		ok := check[c]
+		if ok == 1 {
+			return false
+		}
+		check[c] = 1
+	}
+	return true
+}
+
+func IsUniqueChars3(input string) bool {
+	checker := 0
+	for i, c := range input {
+		val := c - 'a'
+		if (checker & (i << val)) > 0 {
+			return false
+		}
+		checker |= 1 << val
+	}
+	return true
 }
---
+++ algorithm/cracking_the_coding_interview/X.Solutions/Chapter01/1.1_IsUnique/1.1_IsUnique_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//func TestString(t *testing.T) {
+//	fmt.Println('s')
+//	fmt.Println('a')
+//	fmt.Println(int('b'))
+//	fmt.Println(int('c'))
+//	fmt.Println(int('d'))
+//	fmt.Println(int('e'))
+//	fmt.Println(int('f'))
+//}
+//
+//func TestIsUniqueChars(t *testing.T) {
+//	result := IsUniqueChars("abcdefghijklmnopqrstuvwxyzg")
+//	assert.Equal(t, false, result)
+//	result = IsUniqueChars("abcdefghijklmnopqrstuvwxyz")
+//	assert.Equal(t, true, result)
+//	result = IsUniqueChars("qwertyuiopasdfghjklzxcvbnma")
+//	assert.Equal(t, false, result)
+//}
+//
+//func TestIsUniqueChars2(t *testing.T) {
+//	result := IsUniqueChars2("abcdefghijklmnopqrstuvwxyzg")
+//	assert.Equal(t, false, result)
+//	result = IsUniqueChars2("abcdefghijklmnopqrstuvwxyz")
+//	assert.Equal(t, true, result)
+//	result = IsUniqueChars2("qwertyuiopasdfghjklzxcvbnma")
+//	assert.Equal(t, false, result)
+//}
+//
+//func TestIsUniqueChars3(t *testing.T) {
+//	//result := IsUniqueChars3("1234567890abcdefghijklmnopqrstuvwxyz")
+//	result := IsUniqueChars3("abcdefghijklmnopqrstuvwxyzg")
+//	assert.Equal(t, false, result)
+//	result = IsUniqueChars3("abcdefghijklmnopqrstuvwxyz")
+//	assert.Equal(t, true, result)
+//	result = IsUniqueChars3("qwertyuiopasdfghjklzxcvbnma")
+//	assert.Equal(t, false, result)
+//}
+
+func BenchmarkString(b *testing.B) {
+	assert.Equal(b, int32(115), 's')
+	assert.Equal(b, int32(97), 'a')
+	assert.Equal(b, int32(98), 'b')
+	assert.Equal(b, int32(99), 'c')
+	assert.Equal(b, int32(46), '.')
+}
+
+func BenchmarkIsUniqueChars1(b *testing.B) {
+	for i := 0; i < 100000; i++ {
+		result := IsUniqueChars1("abcdefghijklmnopqrstuvwxyzg")
+		assert.Equal(b, false, result)
+		result = IsUniqueChars1("abcdefghijklmnopqrstuvwxyz")
+		assert.Equal(b, true, result)
+		result = IsUniqueChars1("qwertyuiopasdfghjklzxcvbnma")
+		assert.Equal(b, false, result)
+	}
+}
+
+func BenchmarkIsUniqueChars2(b *testing.B) {
+	for i := 0; i < 100000; i++ {
+		result := IsUniqueChars2("abcdefghijklmnopqrstuvwxyzg")
+		assert.Equal(b, false, result)
+		result = IsUniqueChars2("abcdefghijklmnopqrstuvwxyz")
+		assert.Equal(b, true, result)
+		result = IsUniqueChars2("qwertyuiopasdfghjklzxcvbnma")
+		assert.Equal(b, false, result)
+	}
+}
+
+func BenchmarkIsUniqueChars3(b *testing.B) {
+	for i := 0; i < 100000; i++ {
+		//result := IsUniqueChars3("1234567890abcdefghijklmnopqrstuvwxyz")
+		result := IsUniqueChars3("abcdefghijklmnopqrstuvwxyzg")
+		assert.Equal(b, false, result)
+		result = IsUniqueChars3("abcdefghijklmnopqrstuvwxyz")
+		assert.Equal(b, true, result)
+		result = IsUniqueChars3("qwertyuiopasdfghjklzxcvbnma")
+		assert.Equal(b, false, result)
+	}
+}
