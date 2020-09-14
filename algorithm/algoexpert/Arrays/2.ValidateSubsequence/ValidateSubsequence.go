a6da1cde9e63ca2d6453f9261c11454d4fe69823
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2020-09-14 21:45:17 +0800

    add ValidateSubsequence

--- algorithm/algoexpert/Arrays/2.ValidateSubsequence/ValidateSubsequence.go
+++ algorithm/algoexpert/Arrays/2.ValidateSubsequence/ValidateSubsequence.go
@@ -1,5 +1,21 @@
 package ValidateSubsequence
 
 func IsValidSubsequence(array []int, sequence []int) bool {
-	return false
+	check := make(map[int]int)
+	for _, v := range array {
+		_, ok := check[v]
+		if ok {
+			check[v] = check[v] + 1
+		} else {
+			check[v] = 1
+		}
+	}
+	for _, v := range sequence {
+		result, ok := check[v]
+		if !ok || result == 0 {
+			return false
+		}
+		check[v] = check[v] - 1
+	}
+	return true
 }
--- algorithm/algoexpert/Arrays/2.ValidateSubsequence/ValidateSubsequence_test.go
+++ algorithm/algoexpert/Arrays/2.ValidateSubsequence/ValidateSubsequence_test.go
@@ -3,8 +3,21 @@ package ValidateSubsequence
 import "testing"
 
 func TestIsValidSubsequence(t *testing.T) {
-	result := IsValidSubsequence([]int{5, 1, 22, 25, 6, -1, 8, 10}, []int{1, 6, -1, 10})
-	if !result {
+	result := false
+	//result = IsValidSubsequence([]int{5, 1, 22, 25, 6, -1, 8, 10}, []int{1, 6, -1, 10})
+	//if !result {
+	//	t.Fatalf("%v", result)
+	//}
+	//result = IsValidSubsequence([]int{5, 1, 22, 25, 6, -1, 8, 10}, []int{5, 1, 22, 25, 6, -1, 8, 10})
+	//if !result {
+	//	t.Fatalf("%v", result)
+	//}
+	//result = IsValidSubsequence([]int{5, 1, 22, 25, 6, -1, 8, 10}, []int{5, 1, 22, 22, 25, 6, -1, 8, 10})
+	//if result {
+	//	t.Fatalf("%v", result)
+	//}
+	result = IsValidSubsequence([]int{5, 1, 22, 25, 6, -1, 8, 10}, []int{5, 1, 25, 22, 6, -1, 8, 10})
+	if result {
 		t.Fatalf("%v", result)
 	}
 }
