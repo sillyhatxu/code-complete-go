a35d8828682ad09fbbb80bfc9ed5a449c1dda134
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2020-09-16 20:56:15 +0800

    add IsValidSubsequence

--- algorithm/algoexpert/Arrays/2.ValidateSubsequence/ValidateSubsequence.go
+++ algorithm/algoexpert/Arrays/2.ValidateSubsequence/ValidateSubsequence.go
@@ -1,21 +1,17 @@
 package ValidateSubsequence
 
 func IsValidSubsequence(array []int, sequence []int) bool {
-	check := make(map[int]int)
+	if len(array) < len(sequence) {
+		return false
+	}
+	index := 0
 	for _, v := range array {
-		_, ok := check[v]
-		if ok {
-			check[v] = check[v] + 1
-		} else {
-			check[v] = 1
+		if v == sequence[index] {
+			index++
+		}
+		if index == len(sequence) {
+			break
 		}
 	}
-	for _, v := range sequence {
-		result, ok := check[v]
-		if !ok || result == 0 {
-			return false
-		}
-		check[v] = check[v] - 1
-	}
-	return true
+	return index == len(sequence)
 }
