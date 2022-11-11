5b1548257aba5ea15bbcfe62c0cf4893ed413a30
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2022-11-11 23:03:02 +0800

    2022-11-11

---
+++ algorithm/leetcode/101-150/118_Pascal_s_Triangle/118.go
@@ -0,0 +1,5 @@
+package _18_Pascal_s_Triangle
+
+func generate(numRows int) [][]int {
+	return nil
+}
---
+++ algorithm/leetcode/101-150/118_Pascal_s_Triangle/118_test.go
@@ -0,0 +1,14 @@
+package _18_Pascal_s_Triangle
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_generate(t *testing.T) {
+	fmt.Println(0 >> 1)
+	fmt.Println(1 >> 1)
+	fmt.Println(2 >> 1)
+	fmt.Println(3 >> 1)
+	fmt.Println(4 >> 1)
+}
