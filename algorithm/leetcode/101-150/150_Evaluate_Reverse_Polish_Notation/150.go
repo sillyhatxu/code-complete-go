5d3d4c346b170e3d9499904c3eee8c20e2732c31
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2022-11-17 00:23:21 +0800

    2022-11-17

--- algorithm/leetcode/101-150/150_Evaluate_Reverse_Polish_Notation/150.go
+++ algorithm/leetcode/101-150/150_Evaluate_Reverse_Polish_Notation/150.go
@@ -14,29 +14,53 @@ func evalRPN(tokens []string) int {
 			b, _ := strconv.Atoi(tokens[i-1])
 			tokens[i-2] = fmt.Sprintf("%d", a+b)
 			tokens = append(tokens[:i-1], tokens[i+1:]...)
-			i-=2
+			i -= 2
 		case "-":
 			a, _ := strconv.Atoi(tokens[i-2])
 			b, _ := strconv.Atoi(tokens[i-1])
 			tokens[i-2] = fmt.Sprintf("%d", a-b)
 			tokens = append(tokens[:i-1], tokens[i+1:]...)
-			i-=2
+			i -= 2
 		case "*":
 			a, _ := strconv.Atoi(tokens[i-2])
 			b, _ := strconv.Atoi(tokens[i-1])
 			tokens[i-2] = fmt.Sprintf("%d", a*b)
 			tokens = append(tokens[:i-1], tokens[i+1:]...)
-			i-=2
+			i -= 2
 		case "/":
 			a, _ := strconv.Atoi(tokens[i-2])
 			b, _ := strconv.Atoi(tokens[i-1])
 			tokens[i-2] = fmt.Sprintf("%d", a/b)
 			tokens = append(tokens[:i-1], tokens[i+1:]...)
-			i-=2
+			i -= 2
 		}
 	}
 	result, _ := strconv.Atoi(tokens[0])
 	return result
 }
 
-calculator
+func evalRPN1(tokens []string) int {
+	var result []int
+	for i := 0; i < len(tokens); i++ {
+		switch t := tokens[i]; t {
+		case "+", "-", "*", "/":
+			a, b := result[len(result)-2], result[len(result)-1]
+			result = result[:len(result)-2]
+			var res int
+			if t == "+" {
+				res = a + b
+			} else if t == "-" {
+				res = a - b
+			} else if t == "*" {
+				res = a * b
+			} else {
+				res = a / b
+			}
+			result = append(result, res)
+		default:
+			i, _ := strconv.Atoi(t)
+			result = append(result, i)
+		}
+	}
+	return result[0]
+}
