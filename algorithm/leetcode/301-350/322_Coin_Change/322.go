e24a7b14b69b455ec28246ef56a9f71f587b6a2a
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-01-06 00:05:22 +0800

    2023-01-06

--- algorithm/leetcode/301-350/322_Coin_Change/322.go
+++ algorithm/leetcode/301-350/322_Coin_Change/322.go
@@ -15,6 +15,30 @@ func coinChange(coins []int, amount int) int {
 	for i := 1; i <= amount; i++ {
 		dp[i] = total
 		for _, coin := range coins {
+			if i-coin >= 0 && dp[i-coin] != total {
+				dp[i] = min(dp[i-coin]+1, dp[i])
+			}
+		}
+	}
+	if dp[amount] == total {
+		return -1
+	}
+	return dp[amount]
+}
+
+func coinChange1(coins []int, amount int) int {
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+	total := amount + 1
+	dp := make([]int, total, total)
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		dp[i] = total
+		for _, coin := range coins {
 			if (i-coin) >= 0 && dp[i-coin] != total {
 				dp[i] = min(dp[i], dp[i-coin]+1)
 			}
