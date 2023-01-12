a04c699614eb97a4653a2103ddfea557d2260722
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-01-12 21:46:33 +0800

    2023-01-12

--- algorithm/leetcode/101-150/103_Binary_Tree_Zigzag_Level_Order_Traversal/103.go
+++ algorithm/leetcode/101-150/103_Binary_Tree_Zigzag_Level_Order_Traversal/103.go
@@ -5,6 +5,49 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 		return nil
 	}
 	var result [][]int
+	var leftStack []*TreeNode
+	var rightStack []*TreeNode
+	leftStack = append(leftStack, root)
+	for len(leftStack) > 0 || len(rightStack) > 0 {
+		var currentRes []int
+		for len(leftStack) > 0 {
+			currentNode := leftStack[0]
+			leftStack = leftStack[1:]
+			currentRes = append(currentRes, currentNode.Val)
+			if currentNode.Left != nil {
+				rightStack = append([]*TreeNode{currentNode.Left}, rightStack...)
+			}
+			if currentNode.Right != nil {
+				rightStack = append([]*TreeNode{currentNode.Right}, rightStack...)
+			}
+		}
+		if len(currentRes) > 0 {
+			result = append(result, currentRes)
+		}
+		currentRes = []int{}
+		for len(rightStack) > 0 {
+			currentNode := rightStack[0]
+			rightStack = rightStack[1:]
+			currentRes = append(currentRes, currentNode.Val)
+			if currentNode.Right != nil {
+				leftStack = append([]*TreeNode{currentNode.Right}, leftStack...)
+			}
+			if currentNode.Left != nil {
+				leftStack = append([]*TreeNode{currentNode.Left}, leftStack...)
+			}
+		}
+		if len(currentRes) > 0 {
+			result = append(result, currentRes)
+		}
+	}
+	return result
+}
+
+func zigzagLevelOrder1(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	var result [][]int
 	stack1 := []*TreeNode{root}
 	stack2 := []*TreeNode{}
 	for len(stack1) > 0 || len(stack2) > 0 {
--- algorithm/leetcode/101-150/103_Binary_Tree_Zigzag_Level_Order_Traversal/103_test.go
+++ algorithm/leetcode/101-150/103_Binary_Tree_Zigzag_Level_Order_Traversal/103_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func Test_zigzagLevelOrder1(t *testing.T) {
+func Test_zigzagLevelOrderTest(t *testing.T) {
 	test := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	for {
 		fmt.Println(test[len(test)-1])
@@ -18,6 +18,24 @@ func Test_zigzagLevelOrder1(t *testing.T) {
 }
 func Test_zigzagLevelOrder(t *testing.T) {
 	assert.EqualValues(t, [][]int{{1}, {3, 2}, {4, 5}}, zigzagLevelOrder(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 5,
+			},
+		},
+	}))
+}
+
+func Test_zigzagLevelOrder1(t *testing.T) {
+	assert.EqualValues(t, [][]int{{1}, {3, 2}, {4, 5}}, zigzagLevelOrder1(&TreeNode{
 		Val: 1,
 		Left: &TreeNode{
 			Val: 2,
