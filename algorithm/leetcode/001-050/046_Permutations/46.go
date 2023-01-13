0b822b370bc85ca3155dd3b2b576ad9e58c75aa9
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-01-13 21:55:35 +0800

    2023-01-13

--- algorithm/leetcode/001-050/046_Permutations/46.go
+++ algorithm/leetcode/001-050/046_Permutations/46.go
@@ -1,6 +1,24 @@
 package _46_Permutations
 
 func permute(nums []int) [][]int {
+	var dfs func(res *[][]int, currents, candidates []int)
+	dfs = func(res *[][]int, currents, candidates []int) {
+		if len(currents) == 0 {
+			*res = append(*res, candidates)
+			return
+		}
+		for i := 0; i < len(currents); i++ {
+			tempCurrents := append(append([]int{}, currents[0:i]...), currents[i+1:]...)
+			tempCandidates := append(candidates, currents[i])
+			dfs(res, tempCurrents, tempCandidates)
+		}
+	}
+	var res [][]int
+	dfs(&res, nums, []int{})
+	return res
+}
+
+func permute1(nums []int) [][]int {
 	var result [][]int
 	dfs(&result, nums, []int{})
 	return result
--- algorithm/leetcode/001-050/046_Permutations/46_test.go
+++ algorithm/leetcode/001-050/046_Permutations/46_test.go
@@ -1,7 +1,16 @@
 package _46_Permutations
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func Test_permute(t *testing.T) {
-	permute([]int{1, 2, 3})
+	assert.EqualValues(t, [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}, permute([]int{1, 2, 3}))
+	assert.EqualValues(t, [][]int{{0, 1}, {1, 0}}, permute([]int{0, 1}))
+}
+
+func Test_permute1(t *testing.T) {
+	assert.EqualValues(t, [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}, permute1([]int{1, 2, 3}))
+	assert.EqualValues(t, [][]int{{0, 1}, {1, 0}}, permute1([]int{0, 1}))
 }
