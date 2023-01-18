17eeb8b7cb2235388f8eeb6decb96ab80ba1435e
xushikuan <shikuan.xu@advance.ai>
2023-01-18 17:42:47 +0800

    2023-01-18

--- algorithm/leetcode/201-250/240_Search_a_2D_Matrix_II/240.go
+++ algorithm/leetcode/201-250/240_Search_a_2D_Matrix_II/240.go
@@ -1,6 +1,20 @@
 package _40_Search_a_2D_Matrix_II
 
 func searchMatrix(matrix [][]int, target int) bool {
+	row, column := 0, len(matrix[0])-1
+	for row < len(matrix) && column >= 0 {
+		if target == matrix[row][column] {
+			return true
+		} else if target > matrix[row][column] {
+			row++
+		} else if target < matrix[row][column] {
+			column--
+		}
+	}
+	return false
+}
+
+func searchMatrix1(matrix [][]int, target int) bool {
 	i, j := 0, len(matrix[0])-1
 	for i < len(matrix) && j >= 0 {
 		if target == matrix[i][j] {
--- algorithm/leetcode/201-250/240_Search_a_2D_Matrix_II/240_test.go
+++ algorithm/leetcode/201-250/240_Search_a_2D_Matrix_II/240_test.go
@@ -6,6 +6,13 @@ import (
 )
 
 func Test_searchMatrix(t *testing.T) {
+	assert.EqualValues(t, true, searchMatrix([][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}, 19))
 	assert.EqualValues(t, true, searchMatrix([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5))
 	assert.EqualValues(t, false, searchMatrix([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20))
+}
+
+func Test_searchMatrix1(t *testing.T) {
+	assert.EqualValues(t, true, searchMatrix1([][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}, 19))
+	assert.EqualValues(t, true, searchMatrix1([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5))
+	assert.EqualValues(t, false, searchMatrix1([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20))
 }
