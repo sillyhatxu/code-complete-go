df94e806e227001939479a4f8d83c1e2c8b68be2
xushikuan <shikuan.xu@advance.ai>
2023-01-19 17:20:07 +0800

    2023-01-19

--- algorithm/leetcode/101-150/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/106.go
+++ algorithm/leetcode/101-150/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/106.go
@@ -1,6 +1,26 @@
 package _06_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal
 
 func buildTree(inorder []int, postorder []int) *TreeNode {
+	inordeIndexrMap := make(map[int]int, len(inorder))
+	for i := 0; i < len(inorder); i++ {
+		inordeIndexrMap[inorder[i]] = i
+	}
+	var helper func(inorder []int, postorder []int, postIndex, inStart, inEnd int, inordeIndexrMap map[int]int) *TreeNode
+	helper = func(inorder []int, postorder []int, postIndex, inStart, inEnd int, inordeIndexrMap map[int]int) *TreeNode {
+		if inStart > inEnd {
+			return nil
+		}
+		rootIndex := inordeIndexrMap[postorder[postIndex]]
+		return &TreeNode{
+			Val:   postorder[postIndex],
+			Left:  helper(inorder, postorder, postIndex-(inEnd-rootIndex)-1, inStart, rootIndex-1, inordeIndexrMap),
+			Right: helper(inorder, postorder, postIndex-1, rootIndex+1, inEnd, inordeIndexrMap),
+		}
+	}
+	return helper(inorder, postorder, len(postorder)-1, 0, len(inorder)-1, inordeIndexrMap)
+}
+
+func buildTree1(inorder []int, postorder []int) *TreeNode {
 	inorderMap := make(map[int]int, len(inorder))
 	for i := 0; i < len(inorder); i++ {
 		inorderMap[inorder[i]] = i
--- algorithm/leetcode/101-150/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/106_test.go
+++ algorithm/leetcode/101-150/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/106_test.go
@@ -5,19 +5,34 @@ import (
 	"testing"
 )
 
-func Test_buildTree1(t *testing.T) {
-	result := buildTree([]int{3, 2, 1}, []int{3, 2, 1})
-	assert.EqualValues(t, 1, result.Val)
-	assert.EqualValues(t, 2, result.Left.Val)
-	assert.EqualValues(t, 3, result.Left.Left.Val)
-}
 func Test_buildTree(t *testing.T) {
-	result := buildTree([]int{9, 3, 15, 20, 7}, []int{9, 15, 7, 20, 3})
-	assert.EqualValues(t, 3, result.Val)
-	assert.EqualValues(t, 9, result.Left.Val)
-	assert.Nil(t, result.Left.Left)
-	assert.Nil(t, result.Left.Right)
-	assert.EqualValues(t, 20, result.Right.Val)
-	assert.EqualValues(t, 15, result.Right.Left.Val)
-	assert.EqualValues(t, 7, result.Right.Right.Val)
+	result1 := buildTree([]int{3, 2, 1}, []int{3, 2, 1})
+	assert.EqualValues(t, 1, result1.Val)
+	assert.EqualValues(t, 2, result1.Left.Val)
+	assert.EqualValues(t, 3, result1.Left.Left.Val)
+
+	result2 := buildTree([]int{9, 3, 15, 20, 7}, []int{9, 15, 7, 20, 3})
+	assert.EqualValues(t, 3, result2.Val)
+	assert.EqualValues(t, 9, result2.Left.Val)
+	assert.Nil(t, result2.Left.Left)
+	assert.Nil(t, result2.Left.Right)
+	assert.EqualValues(t, 20, result2.Right.Val)
+	assert.EqualValues(t, 15, result2.Right.Left.Val)
+	assert.EqualValues(t, 7, result2.Right.Right.Val)
+}
+
+func Test_buildTree1(t *testing.T) {
+	result1 := buildTree1([]int{3, 2, 1}, []int{3, 2, 1})
+	assert.EqualValues(t, 1, result1.Val)
+	assert.EqualValues(t, 2, result1.Left.Val)
+	assert.EqualValues(t, 3, result1.Left.Left.Val)
+
+	result2 := buildTree1([]int{9, 3, 15, 20, 7}, []int{9, 15, 7, 20, 3})
+	assert.EqualValues(t, 3, result2.Val)
+	assert.EqualValues(t, 9, result2.Left.Val)
+	assert.Nil(t, result2.Left.Left)
+	assert.Nil(t, result2.Left.Right)
+	assert.EqualValues(t, 20, result2.Right.Val)
+	assert.EqualValues(t, 15, result2.Right.Left.Val)
+	assert.EqualValues(t, 7, result2.Right.Right.Val)
 }
