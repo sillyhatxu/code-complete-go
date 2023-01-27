ca2274bc9f7501d7bb501957d38d72ec7381a013
xushikuan <shikuan.xu@advance.ai>
2023-01-27 18:13:54 +0800

    2023-01-27

--- algorithm/leetcode/151-200/152_Maximum_Product_Subarray/152.go
+++ algorithm/leetcode/151-200/152_Maximum_Product_Subarray/152.go
@@ -1,6 +1,29 @@
 package _52_Maximum_Product_Subarray
 
 func maxProduct(nums []int) int {
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+	res, maxValue, minValue := nums[0], nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		tempMax := maxValue
+		maxValue = max(maxValue*nums[i], max(minValue*nums[i], nums[i]))
+		minValue = min(tempMax*nums[i], min(minValue*nums[i], nums[i]))
+		res = max(res, maxValue)
+	}
+	return res
+}
+
+func maxProduct1(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
--- algorithm/leetcode/151-200/152_Maximum_Product_Subarray/152_test.go
+++ algorithm/leetcode/151-200/152_Maximum_Product_Subarray/152_test.go
@@ -8,3 +8,7 @@ import (
 func Test_maxProduct(t *testing.T) {
 	assert.EqualValues(t, 6, maxProduct([]int{2, 3, -2, 4}))
 }
+
+func Test_maxProduct1(t *testing.T) {
+	assert.EqualValues(t, 6, maxProduct1([]int{2, 3, -2, 4}))
+}
