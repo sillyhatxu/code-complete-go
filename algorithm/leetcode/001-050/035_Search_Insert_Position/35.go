515e2714d0becf5da8959eb32226d632824d5d27
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-01-30 22:28:23 +0800

    2023-01-30

--- algorithm/leetcode/001-050/035_Search_Insert_Position/35.go
+++ algorithm/leetcode/001-050/035_Search_Insert_Position/35.go
@@ -6,6 +6,21 @@ func searchInsert(nums []int, target int) int {
 		mid := l + (r-l)/2
 		if nums[mid] == target {
 			return mid
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
+
+func searchInsert1(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] == target {
+			return mid
 		}
 		if nums[mid] > target {
 			r = mid - 1
