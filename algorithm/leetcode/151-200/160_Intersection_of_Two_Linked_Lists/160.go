a8b492aff85aed3fd203ca8b28babb1d93513940
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-02-03 23:50:16 +0800

    2023-02-03

--- algorithm/leetcode/151-200/160_Intersection_of_Two_Linked_Lists/160.go
+++ algorithm/leetcode/151-200/160_Intersection_of_Two_Linked_Lists/160.go
@@ -1,13 +1,23 @@
 package _60_Intersection_of_Two_Linked_Lists
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	a, b := headA, headB
+	for a != b {
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+	}
+	return a
+}
+
+func getIntersectionNode1(headA, headB *ListNode) *ListNode {
 	if headA == nil || headB == nil {
 		return nil
 	}
