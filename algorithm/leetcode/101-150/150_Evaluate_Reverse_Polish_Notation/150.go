1b52887cc0cc648f8cf69b60318d7c026b0eb576
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-02-08 23:48:13 +0800

    2023-02-08

--- algorithm/leetcode/101-150/150_Evaluate_Reverse_Polish_Notation/150.go
+++ algorithm/leetcode/101-150/150_Evaluate_Reverse_Polish_Notation/150.go
@@ -5,8 +5,32 @@ import (
 	"strconv"
 )
 
-//Time: O(n); Space: O(1)
 func evalRPN(tokens []string) int {
+	var res []int
+	for i := 0; i < len(tokens); i++ {
+		num, err := strconv.Atoi(tokens[i])
+		if err == nil {
+			res = append(res, num)
+			continue
+		}
+		num1, num2 := res[len(res)-2], res[len(res)-1]
+		res = res[:len(res)-2]
+		switch tokens[i] {
+		case "+":
+			res = append(res, num1+num2)
+		case "-":
+			res = append(res, num1-num2)
+		case "*":
+			res = append(res, num1*num2)
+		case "/":
+			res = append(res, num1/num2)
+		}
+	}
+	return res[0]
+}
+
+//Time: O(n); Space: O(1)
+func evalRPN2(tokens []string) int {
 	for i := 0; i < len(tokens); i++ {
 		switch tokens[i] {
 		case "+":
--- algorithm/leetcode/101-150/150_Evaluate_Reverse_Polish_Notation/150_test.go
+++ algorithm/leetcode/101-150/150_Evaluate_Reverse_Polish_Notation/150_test.go
@@ -8,3 +8,11 @@ import (
 func Test_evalRPN(t *testing.T) {
 	assert.EqualValues(t, 22, evalRPN([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}))
 }
+
+func Test_evalRPN2(t *testing.T) {
+	assert.EqualValues(t, 22, evalRPN2([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}))
+}
+
+func Test_evalRPN1(t *testing.T) {
+	assert.EqualValues(t, 22, evalRPN1([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}))
+}
