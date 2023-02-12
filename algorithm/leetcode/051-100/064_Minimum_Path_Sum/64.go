c53b0868d68fa532cf6805fd1a0d1f87b12e19de
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2023-02-12 22:55:59 +0800

    2023-02-12

--- algorithm/leetcode/051-100/064_Minimum_Path_Sum/64.go
+++ algorithm/leetcode/051-100/064_Minimum_Path_Sum/64.go
@@ -7,6 +7,26 @@ func minPathSum(grid [][]int) int {
 		}
 		return b
 	}
+	m, n := len(grid), len(grid[0])
+	for i := 1; i < n; i++ {
+		grid[0][i] += grid[0][i-1]
+	}
+	for i := 1; i < m; i++ {
+		grid[i][0] += grid[i-1][0]
+		for j := 1; j < n; j++ {
+			grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+		}
+	}
+	return grid[m-1][n-1]
+}
+
+func minPathSum1(grid [][]int) int {
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
 	row := len(grid)
 	column := len(grid[0])
 	for c := 1; c < column; c++ {
